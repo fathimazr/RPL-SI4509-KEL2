6119bf541f912432501ef2c57f4738c1e31d8451
ryanmuhs <ryansatria@student.telkomuniversity.ac.id>
2024-03-26 13:29:20 +0700

    fix: fixing error login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,11 +10,6 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            @error('email')
-                <div class="text-red-500 mt-2">
-                    {{ $message }}
-                </div>
-            @enderror
         </div>
 
         <!-- Password -->
