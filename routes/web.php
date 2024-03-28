40e4731510d52fd10c1b5520e9f4d2b34d482199
ryanmuhs <ryansatria@student.telkomuniversity.ac.id>
2024-03-28 14:11:50 +0700

    feat : Membuat TrafoController dan menambahkannya ke routingan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TrafoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard', [
-        "title"=> "Home"
-    ]);
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -26,6 +25,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// routing untuk trafo --> pake resource biar lebih ringkas
+Route::resource('trafo', TrafoController::class)->middleware('auth');
 
 Route::get('/welcome', function () {
     return view('welcome');
