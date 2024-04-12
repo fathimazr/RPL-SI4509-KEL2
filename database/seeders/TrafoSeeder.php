b869f27a2eb15772923168d8b422b246df28b437
ryanmuhs <ryansatria@student.telkomuniversity.ac.id>
2024-04-12 10:40:24 +0700

    feat: Menambah seeder untuk data dummy

---
+++ database/seeders/TrafoSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class TrafoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Data trafo dengan format tanggal dd-mm-yyyy
+        $data = [
+            [
+                'trafo_id' => '1101',
+                'brand' => 'STARLITE',
+                'city' => 'JAKARTA',
+                'phase' => 1,
+                'latitude' => 1.043496628353671,
+                'longitude' => 107.38636884737015,
+                'capacity' => 200,
+                'installation_date' => '3-8-2023' 
+            ]
+        ];
+
+        // Masukkan data trafo ke dalam database
+        foreach ($data as $trafoData) {
+            // Ubah format tanggal ke dalam format yang sesuai dengan database
+            $trafoData['installation_date'] = Carbon::createFromFormat('d-m-Y', $trafoData['installation_date'])->format('Y-m-d');
+
+            // Simpan data trafo ke dalam tabel 'trafos' menggunakan Query Builder
+            DB::table('trafos')->insert($trafoData);
+        }
+    }
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin 1',
+                'email' => 'user1@email.com',
+                'employee_id' => 'admin1',
+                'role' => 'admin',
+                'branch_office' => 'Bandung',
+                'password' => Hash::make('admin1pass')
+            ],
+            // Manager
+            [
+                'name' => 'Manager 1',
+                'email' => 'manager1@email.com',
+                'employee_id' => 'manager1',
+                'role' => 'manager',
+                'branch_office' => 'Bandung',
+                'password' => Hash::make('manager1pass')
+            ],
+            // Teknis
+            [
+                'name' => 'Teknis 1',
+                'email' => 'teknis1@email.com',
+                'employee_id' => 'teknis1',
+                'role' => 'tim_teknis',
+                'branch_office' => 'Bandung',
+                'password' => Hash::make('teknis1pass')
+            ],
+        ]);
+    }
+}
