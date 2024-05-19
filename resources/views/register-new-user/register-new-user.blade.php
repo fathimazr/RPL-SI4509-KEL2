5c98f7c97d2b2698d1935d91c4a152c7551c8657
fathimazr <2003fathimah@gmail.com>
2024-05-19 16:50:23 +0700

    perbaikan di input employee id

--- resources/views/register-new-user/register-new-user.blade.php
+++ resources/views/register-new-user/register-new-user.blade.php
@@ -160,14 +160,13 @@
         }
 
     </style>
-
 </head>
 <!-- <body> -->
 <x-app-layout>
     <div class="main">
         <h1 class="form-title">REGISTER NEW USER</h1>
         <div class="form-header">
-            <img class = "form-header-icon" src = "img/form.png" alt = "Icon" width = "30px">
+            <img class = "form-header-icon" src = "img/Form.png" alt = "Icon" width = "30px">
             <h1 class="form-header-title">Filling Form</h1>
         </div>
         <div class="form-container">
@@ -175,17 +174,8 @@
         <form id="form" action="{{route('admin.regist')}}" method="POST"> 
                 @csrf
                 <div class="form-group">
-                <label for="employeeID">Employee ID</label>
-                <select class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="employeeID" name="employee_id" type="text" placeholder="">
-                    <option value="" disabled selected>Select your employee id</option>
-                    @foreach($enumValues as $id)
-                        @if(in_array($id, $usedEmployeeIds))
-                            <option value="{{ $id }}" disabled>{{ $id }} (already used)</option>
-                        @else
-                            <option value="{{ $id }}">{{ $id }}</option>
-                        @endif
-                    @endforeach
-                </select>
+                    <label for="employeeID">Employee ID</label>
+                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="employeeID" name = "employee_id" type="text" placeholder="Please fill with your employee ID">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
@@ -203,6 +193,7 @@
                         <option value="" disabled selected>Select your role</option>
                         <option value="manager">Manajer</option>
                         <option value="tim_teknis">Tim Teknis</option>
+                        <option value="admin">Admin</option>
                     </select>
                 </div>
                 <div class="form-group">
@@ -224,7 +215,6 @@
                 </div>
             </form>
             </div>
-            
             <script>
 
     document.getElementById('discardButton').addEventListener('click', function() {
